8504ab0bdd0862547e7beafc6e076a4f9501210f
cosine0720 <cosine0720@gmail.com>
2022-10-13 11:58:26 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`This app is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`This app is running on http://localhost:${PORT}`)
 })
